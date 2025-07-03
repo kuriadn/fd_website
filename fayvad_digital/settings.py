be69d7dbca8912f9050fbfdc79dbdbaa872be9be
kuriadn <dn.kuria@gmail.com>
2025-07-03 17:41:21 +0300

    modified:   fayvad_digital/settings.py 	modified:   fayvad_digital/urls.py

--- fayvad_digital/settings.py
+++ fayvad_digital/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -100,7 +99,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
@@ -147,3 +145,8 @@ CKEDITOR_CONFIGS = {
         'width': '100%',
     },
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://digital.fayvad.com',
+    'https://www.digital.fayvad.com',
+]
--- fayvad_digital/urls.py
+++ fayvad_digital/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
          name='django.contrib.sitemaps.views.sitemap'),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 # Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
